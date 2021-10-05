e3ced53bb8058d5c275f3d579ec1421bb6a278b0
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-10-05 15:09:15 -0400

    fourth problem, first attempt

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+digitCheck = number[-1:]
+if (digitCheck > 5):
+    print("Last digit of {:d} is {:d} and is greater than 5",
+          number, digitCheck)
+elif (digitCheck == 0):
+    print("Last digit of {:d} is {:d} and is 0", number, digitCheck)
+else:
+    print(
+        "Last digit of {:d} is {:d} and is less than 6 and not 0", number, digitCheck)
