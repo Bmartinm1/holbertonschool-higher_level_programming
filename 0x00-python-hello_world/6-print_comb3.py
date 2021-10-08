92d77d87c0978554364901818cf382dc3d364b4d
Ben MartinMooney <ben.martinmooney@gmail.com>
2021-10-08 14:42:27 -0400

    solved

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+for i in range(0, 10):
+    for x in range(0, 10):
+        if i == 8 and x == 9:
+            print("89")
+        elif i < x:
+            print("{:d}{:d}, ".format(i, x), end="")
